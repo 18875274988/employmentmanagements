1cc1fc2ec55d20583a43b6b6c3e7aed65ca48a4c
puyinsheng <tydic220ldap>
2020-04-24 21:56:25 +0800

    Initial commit

---
+++ src/main/java/com/em/employmentmanagements/EmploymentmanagementsApplication.java
@@ -0,0 +1,13 @@
+package com.em.employmentmanagements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentmanagementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentmanagementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/em/employmentmanagements/EmploymentmanagementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.em.employmentmanagements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentmanagementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
