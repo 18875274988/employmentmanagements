71c78c0ae1f62af60f007119dc8c695bfc915898
puyinsheng <tydic220ldap>
2020-04-24 22:14:43 +0800

    1

--- src/main/java/com/em/employmentmanagements/EmploymentmanagementsApplication.java
+++ src/main/java/com/em/employmentmanagements/EmploymentmanagementsApplication.java
@@ -1,8 +1,9 @@
 package com.em.employmentmanagements;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.em.employmentmanagements.mapper")
 @SpringBootApplication
 public class EmploymentmanagementsApplication {
 
--- src/main/java/com/em/employmentmanagements/mapper/UserMapper.java
+++ src/main/java/com/em/employmentmanagements/mapper/UserMapper.java
@@ -1,10 +1,13 @@
 package com.em.employmentmanagements.mapper;
 
+import org.springframework.stereotype.Repository;
+
 /**
  * 描述：
  *
  * @author PuYinsheng
  * @date 2020/4/24
  **/
+@Repository
 public class UserMapper {
 }
